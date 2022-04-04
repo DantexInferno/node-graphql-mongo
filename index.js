e7859405ec4e420f21f82aa6a55937eb9b4b67f6
Julio Diaz <gorilamc@gmail.com>
2022-04-04 15:53:57 -0500

    creation project node graphql and mongo db

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+require('dotenv').config()
+const { makeExecutableSchema } = require('graphql-tools')
+const cors = require('cors')
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.PORT || 3000
+const isDev = process.env.NODE_ENV !== 'production'
+
+console.log(isDev);
+
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
+)
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+app.use(cors())
+
+app.use('/api', graphqlHTTP({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: isDev
+}))
+
+app.listen(port, () => {
+  console.log('Server UP')
+})
